94954c2384f72cf87db49455b04cd40b437baa1f
NikKasatkin <i_human@list.ru>
2018-08-26 13:04:04 +0400

    ALL done

--- src/main/java/mailru_page.java
+++ src/main/java/mailru_page.java
@@ -1,21 +1,12 @@
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import io.qameta.allure.Attachment;
-import io.qameta.allure.Severity;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
-import org.eclipse.sisu.Description;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ByIdOrName;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
-import sun.print.DialogOnTop;
-//import ru.yandex.qatools.allure.*;
 
 import javax.swing.*;
 import java.net.MalformedURLException;
@@ -23,95 +14,118 @@ import java.net.URL;
 import java.util.List;
 
 
+
 @Story("Используем CHROME для автотеста")
-public class mailru_page  {
-
-    public String  URL = "http://192.168.0.82:4444/wd/hub";
-
-@Step
-@Story("Вход в Gmail")
-
-    public mailru_page logging_in ( String user, String password, String searching_name) throws InterruptedException, MalformedURLException {
-
-        //настройка GRIT
-        //java -jar selenium-server-standalone-3.14.0.jar -role hub                                                -hub
-        //java -jar selenium-server-standalone-3.14.0.jar -role node  -hub http://localhost:4444/grid/register     -local
-        //java -jar selenium-server-standalone-3.14.0.jar -role node  -hub http://192.168.0.82:4444/grid/register  -web
+public class mailru_page {
 
 
-        DesiredCapabilities cap = DesiredCapabilities.chrome();
-        cap.setPlatform(Platform.WINDOWS);
-        URL url = new URL(URL); // http://localhost:4444/wd/hub
+    //настройка GRIT и запрос Allure
+    //java -jar selenium-server-standalone-3.14.0.jar -role hub                                                -hub
+    //java -jar selenium-server-standalone-3.14.0.jar -role node  -hub http://localhost:4444/grid/register     -local
+    //java -jar selenium-server-standalone-3.14.0.jar -role node  -hub http://192.168.0.82:4444/grid/register  -web
+    //allure serve allure-results
 
-        WebDriver driver = new RemoteWebDriver(url,cap);
+    //public  WebDriver driver1;
+    public String URL = "http://192.168.0.82:4444/wd/hub";
+
+    public DesiredCapabilities cap = DesiredCapabilities.chrome();
+    public URL url; // http://localhost:4444/wd/hub
+        {
+            try {
+                url = new URL(URL);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    public WebDriver driver = new RemoteWebDriver(url, cap);
+
+
+    @Step
+    @Story("Вход в Gmail")
+
+    public mailru_page logging_in(String user, String password, String searching_name) throws InterruptedException, MalformedURLException {
 
         try {  // for helpful
 
-            //login
-            driver.get("https://accounts.google.com/");
-            driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys(user);
-            driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content/span")).click();
+        //login
+        driver.get("https://accounts.google.com/");
+        driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys(user);
+        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content/span")).click();
 
-            //pass
-            WebDriverWait wait = new WebDriverWait(driver, 100);
-            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
-            WebElement elementpwd = driver.findElement(By.xpath("//input[@type='password']"));
-            elementpwd.sendKeys(password);
-            driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/content/span")).click();
+        //pass
+        WebDriverWait wait = new WebDriverWait(driver, 100);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+        WebElement elementpwd = driver.findElement(By.xpath("//input[@type='password']"));
+        elementpwd.sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/content/span")).click();
 
-            //add gmail in menu
-            WebDriverWait wait1 = new WebDriverWait(driver, 100);
-            wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"gbwa\"]")));
-            driver.findElement(By.xpath("//*[@id=\"gbwa\"]")).click();
-            driver.findElement(By.xpath("//*[@id=\"gb23\"]")).click();
+        //add gmail in menu
+        WebDriverWait wait1 = new WebDriverWait(driver, 100);
+        wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"gbwa\"]")));
+        driver.findElement(By.xpath("//*[@id=\"gbwa\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"gb23\"]")).click();
+        }catch(Exception e){
 
-            //selection menu
-            WebDriverWait waitGmail = new WebDriverWait(driver, 100);
-            waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"aso_search_form_anchor\"]/button[2]")));
-            driver.findElement(By.xpath("//*[@id=\"aso_search_form_anchor\"]/button[2]")).click();
-            waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"ZH nr aQa\"]")));
-            driver.findElement(By.xpath("//*[@class=\"ZH nr aQa\"]")).sendKeys(searching_name);
-            driver.findElement(By.xpath("//*[@data-tooltip=\"Поиск почты\"]")).click();
-
-            //take a count letters
-            List<WebElement> rows_table = driver.findElement(By.xpath("//*[@class=\"F cf zt\"]/tbody")).findElements(By.xpath("//*[@email=\""+searching_name+"\"]"));
-            int rows_count = rows_table.size();
-            long result = rows_count / 2; // on row we have a two email , that we are / 2
-
-            //message on window
-            final JDialog dialog = new JDialog();
-            dialog.setAlwaysOnTop(true);// on top window
-            if (result != 0) {
-                driver.findElement(By.xpath("//*[@class=\"z0\"]/div")).click();
-                waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"vO\"]")));
-                driver.findElement(By.xpath("//*[@class=\"vO\"]")).sendKeys(searching_name);
-                driver.findElement(By.xpath("//*[@name=\"subjectbox\"]")).sendKeys("Тестовое задание от Касаткина Николая");
-                driver.findElement(By.xpath("//*[@class=\"Am Al editable LW-avf\"]")).sendKeys("колличество писем = "+result);
-                driver.findElement(By.xpath("//*[@class=\"J-J5-Ji btA\"]")).click();
-                JOptionPane.showMessageDialog(dialog, "Писем от  " + searching_name + "  " + result + " шт.");
-            }
-            driver.quit();
-
-        }catch (Exception e) {
-
-            //exeption logic
+        //exeption logic
             final JDialog dialog = new JDialog();
             dialog.setAlwaysOnTop(true);
             JOptionPane.showMessageDialog(dialog, "ошибка логики  необходимо освободить node");
             driver.quit();
         }
-
-
-
-        //clean nodes on hub
-        //driver.quit();
-        return null;
-        }
-
-
-
+        return catch_row_chrome(searching_name);
     }
+
+    @Step
+    @Story("Определение колличества писем от закащика")
+    public mailru_page catch_row_chrome(String searching_name) {
+        //selection menu
+        WebDriverWait waitGmail = new WebDriverWait(driver, 100);
+        waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"aso_search_form_anchor\"]/button[2]")));
+        driver.findElement(By.xpath("//*[@id=\"aso_search_form_anchor\"]/button[2]")).click();
+        waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"ZH nr aQa\"]")));
+        driver.findElement(By.xpath("//*[@class=\"ZH nr aQa\"]")).sendKeys(searching_name);
+        driver.findElement(By.xpath("//*[@data-tooltip=\"Поиск почты\"]")).click();
+
+        //take a count letters
+        List<WebElement> rows_table = driver.findElement(By.xpath("//*[@class=\"F cf zt\"]/tbody")).findElements(By.xpath("//*[@email=\"" + searching_name + "\"]"));
+        int rows_count = rows_table.size();
+        long result = rows_count / 2; // on row we have a two email , that we are / 2
+
+        //message on window
+        //        final JDialog dialog = new JDialog();
+        //        dialog.setAlwaysOnTop(true);// on top window
+        //        JOptionPane.showMessageDialog(dialog, "Писем от  " + searching_name + "  " + result + " шт.");
+        return answer_letter(result,searching_name);
+    }
+
+    @Step
+    @Story("Отправка обратно письма закащику")
+    public mailru_page answer_letter (long result, String searching_name){
+            if (result != 0) {
+                driver.findElement(By.xpath("//*[@class=\"z0\"]/div")).click();
+                WebDriverWait waitGmail = new WebDriverWait(driver, 100);
+                waitGmail.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class=\"vO\"]")));
+                driver.findElement(By.xpath("//*[@class=\"vO\"]")).sendKeys(searching_name);
+                driver.findElement(By.xpath("//*[@name=\"subjectbox\"]")).sendKeys("Тестовое задание от Касаткина Николая");
+                driver.findElement(By.xpath("//*[@class=\"Am Al editable LW-avf\"]")).sendKeys("колличество писем = " + result);
+                driver.findElement(By.xpath("//*[@class=\"J-J5-Ji btA\"]")).click();
+                final JDialog dialog = new JDialog();
+                dialog.setAlwaysOnTop(true);
+                JOptionPane.showMessageDialog(dialog, "Писем от  " + searching_name + "  " + result + " шт.");
+            }else {
+                final JDialog dialog1 = new JDialog();
+                dialog1.setAlwaysOnTop(true);
+                JOptionPane.showMessageDialog(dialog1, "ошибка логики  необходимо освободить node");
+                driver.quit();
+
+            }
+        driver.quit();
+        return null;
+        //allure serve allure-results
+    }
+}
 
 
 
--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,17 +1,16 @@
-import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import org.eclipse.sisu.Description;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
 @Epic("Авто Тест GMAIL")
 @Feature("Gmail на CHROME ")
+
+
 public class test1 {
-    @Test (description = "Тестовое задание на вакансию" +
+
+    @Test(description = "Тестовое задание на вакансию" +
             "Разработчик в тестировании")
     @io.qameta.allure.Description("Необходимо:\n\n" +
             "        1) Использовать Python/Java/C#, подключить библиотеку Selenium Webdriver;\n\n" +
@@ -20,15 +19,13 @@ public class test1 {
             "        4) с помощью Selenium определить, сколько нашлось писем;\n\n" +
             "        5) С помощью Selenium и интерфейса почты автоматически написать и отправить письмо Филинину Илье,\n\n" +
             "        в тексте которого указать найденное в шаге 4 количество писем. Указать тему")
-
     public void login() throws InterruptedException, MalformedURLException {
 
-
-        mailru_page mailru_page1 =  new mailru_page();
-        mailru_page1.logging_in("i.human.new@gmail.com","1gctugfags", "i_human@list.ru" ); //i_lead_master@list.ru : i_human@list.ru
-
+        mailru_page mailru_page1 = new mailru_page();
+        mailru_page1.logging_in("i.human.new@gmail.com", "1gctugfags", "i_human@list.ru"); //i_lead_master@list.ru : i_human@list.ru
 
 
     }
+
 }
 
